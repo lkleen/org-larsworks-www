119ab965ee3c275d5df3e175515ab926cedc8799
Lars Kleen <lars.kleen@retrosounds.de>
2014-01-06 20:11:46 +0100

    added dispatcher configuration

---
+++ src/main/java/org/larsworks/www/config/DispatcherServletConfig.java
@@ -0,0 +1,13 @@
+package org.larsworks.www.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Lars Kleen
+ * @since 0.0.1
+ *        Date: 06.01.14
+ *        Time: 20:06
+ */
+@Configuration
+public class DispatcherServletConfig {
+}
