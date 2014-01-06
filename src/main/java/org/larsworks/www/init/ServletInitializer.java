c63c3d7c4831116ecf78c04533e554ff37b20d1e
Lars Kleen <lars.kleen@retrosounds.de>
2014-01-06 20:02:08 +0100

    added initializer and according dependencies

---
+++ src/main/java/org/larsworks/www/init/ServletInitializer.java
@@ -0,0 +1,20 @@
+package org.larsworks.www.init;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/**
+ * @author Lars Kleen
+ * @since 0.0.1
+ *        Date: 06.01.14
+ *        Time: 19:55
+ */
+public class ServletInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
