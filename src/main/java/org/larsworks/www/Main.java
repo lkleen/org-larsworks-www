ceb0f8dbda6f931386c339b48b366de696ea7432
Lars Kleen <github@larsworks.org>
2014-04-06 18:33:11 +0200

    added primefaces webapp template

---
+++ src/main/java/org/larsworks/www/Main.java
@@ -0,0 +1,13 @@
+package org.larsworks.www;
+
+/**
+ * @author Lars Kleen
+ * @since 0.0.1
+ *        Date: 06.04.14
+ *        Time: 17:56
+ */
+public class Main {
+    public static void main(String... args) {
+
+    }
+}
---
+++ src/main/java/org/larsworks/www/beans/Contact.java
@@ -0,0 +1,12 @@
+package org.larsworks.www.beans;
+
+import java.io.Serializable;
+
+/**
+ * @author Lars Kleen
+ * @since 0.0.1
+ *        Date: 06.04.14
+ *        Time: 18:00
+ */
+public class Contact implements Serializable {
+}
